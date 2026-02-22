31a766ac321ac7d3602b6c8209385a9c89e77337
מרים <miriam@example.com>
2026-02-22 17:02:25 +0200

    הוספת repairRouter מלא עם כל הפונקציות

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import newWigRouter from './Routers/newWigRouter';
-import serviceRouter from './Models_Service/SalonServices/serviceRoutes';
+import serviceRouter from './Routers/serviceRouter';
 import userRouter from './Routers/userRouter';
+import repairRouter from './Routers/repairRouter';
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use('/api/wigs', newWigRouter);
 app.use('/api/services', serviceRouter);
 app.use('/api/users', userRouter);
+app.use('/api/repairs', repairRouter);
 
 export default app;
